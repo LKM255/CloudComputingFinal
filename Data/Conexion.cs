31d531471cbfe6be5c12b6da2a608d071119493b
jean <jean@DESKTOP-CCDAJ7G>
2021-10-31 00:29:48 -0500

    a

---
+++ Data/Conexion.cs
@@ -0,0 +1,44 @@
+﻿using CloudComputingFinal.Models;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CloudComputingFinal.Data
+{
+    public class Conexion
+    {
+        SqlCommand comando;
+        SqlConnection con;
+        SqlDataReader reader;
+
+        public Datos BuscarDNI(string dni)
+        {
+            //List<Datos> listdat = new List<Datos>();
+            Datos dat = new Datos();
+
+            con = new SqlConnection("Server=tcp:buscardnidbserver.database.windows.net,1433;Initial Catalog=BuscarDni_db;Persist Security Info=False;" +
+                "User ID=jeanpierre;Password=Jean2551;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            string fd = "select * from Datos where DNI='" + dni + "'";
+            comando = new SqlCommand(fd, con);
+            con.Open();
+            reader = comando.ExecuteReader();
+
+            bool hay = reader.Read();
+            if (hay)
+            {
+                dat.Nombre = reader[1].ToString();
+                dat.Apellido = reader[2].ToString();
+                //listdat.Add(dat);
+            }
+            else
+            {
+                dat.Nombre = "No hay resultado";
+                dat.Apellido = "";
+                //listdat.Add(dat);
+            }
+            return dat;
+        }
+    }
+}
---
+++ Models/Datos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CloudComputingFinal.Models
+{
+    public class Datos
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+
+        public string DNI { get; set; }
+    }
+}
